017500612c15cdd657251f3004f947883ca0c884
AparisBlink <61824533+AparisBlink@users.noreply.github.com>
2021-03-11 16:36:50 +0100

    Update style.js

--- style.js
+++ style.js
@@ -54,10 +54,10 @@
 				{ name: 'Box 21 Project ', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-21' } },
 
 
-				{ name: 'L.123 Pink ', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-01' } },
-				{ name: 'L.123 Black', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-02' } },
-				{ name: 'L.abc black', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-03' } },
-				{ name: 'L.abc pink', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-04' } },
+				{ name: 'L.123 Pink Left', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-01' } },
+				{ name: 'L.abc Black', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-02' } },
+				{ name: 'L.abc pink', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-03' } },
+				{ name: 'L.123 pink', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-04' } },
 
 				{ name: 'U.List 01', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-01' } },
 				{ name: 'U.List 02', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-02' } }
